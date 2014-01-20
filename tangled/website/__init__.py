79a5d0c59ba0f7e90eba9b6700302ed87f157a3c
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-01-19 23:29:59 -0800

    Mount docs directories as "remote" in production

--- tangled/website/__init__.py
+++ tangled/website/__init__.py
@@ -14,12 +14,13 @@ def make_app(settings):
         if name.startswith('tangled'):
             repos[name] = os.path.join(src_dir, name)
 
+    remote = app.get_setting('env') == 'production'
     for name, directory in repos.items():
         docs_dir = os.path.join(directory, 'docs/_build')
         if os.path.exists(docs_dir):
             prefix = posixpath.join('docs', name)
             app.mount_static_directory(
-                prefix, docs_dir, index_page='index.html')
+                prefix, docs_dir, remote=remote, index_page='index.html')
 
     app.mount_resource('docs', '.resources:Docs', '/docs')
     app.scan('.resources')
