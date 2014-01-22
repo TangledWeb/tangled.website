8ad30f7fb43900274f85ec0498d8e0b129a40043
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-01-21 18:15:24 -0800

    Do scans and mount static dirs and resources via settings

--- tangled/website/__init__.py
+++ tangled/website/__init__.py
@@ -2,22 +2,25 @@ import os
 import posixpath
 
 from tangled.util import abs_path
+from tangled.web import Application
 
 
-def include(app):
-    repos = {}
-    src_dir = abs_path(app.settings['src_dir'])
+def make_app(settings):
+    # TODO: This could be simplified if the docs were built into the
+    # same top level directory. Then we wouldn't need to look at the
+    # repo directories.
+    static_dirs = []
+    remote = settings.get('env') == 'production'
+    src_dir = abs_path(settings['src_dir'])
     for name in os.listdir(src_dir):
         if name.startswith('tangled'):
-            repos[name] = os.path.join(src_dir, name)
-
-    remote = app.get_setting('env') == 'production'
-    for name, directory in repos.items():
-        docs_dir = os.path.join(directory, 'docs/_build')
-        if os.path.exists(docs_dir):
-            prefix = posixpath.join('docs', name)
-            app.mount_static_directory(
-                prefix, docs_dir, remote=remote, index_page='index.html')
-
-    app.mount_resource('docs', '.resources:Docs', '/docs')
-    app.scan('.resources')
+            repo_dir = os.path.join(src_dir, name)
+            docs_dir = os.path.join(repo_dir, 'docs/_build')
+            if os.path.exists(docs_dir):
+                prefix = posixpath.join('docs', name)
+                static_dirs.append({
+                    'args': (prefix, docs_dir),
+                    'kwargs': {'remote': remote, 'index_page': 'index.html'},
+                })
+    settings['tangled.app.static_directories'] = static_dirs
+    return Application(settings)
