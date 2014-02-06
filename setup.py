9266573d2a8dcf658fe6674f16a6787f3792cf33
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-05 18:16:34 -0800

    Update package configuration, pt. ii

--- setup.py
+++ setup.py
@@ -9,20 +9,22 @@ setup(
     url='http://tangledframework.org/',
     author='Wyatt Baldwin',
     author_email='self@wyattbaldwin.com',
-    packages=find_packages(),
-    install_requires=(
+    packages=[
+        'tangled.website',
+    ],
+    install_requires=[
         'tangled.auth>=0.1.dev0',
         'tangled.session>=0.1.dev0',
         'tangled.site>=0.1.dev0',
         'SQLAlchemy>=0.9.1',
-    ),
+    ],
     extras_require={
-        'dev': ('coverage',),
+        'dev': ['coverage'],
     },
-    classifiers=(
+    classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-    ),
+    ],
 )
